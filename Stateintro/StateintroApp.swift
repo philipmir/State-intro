124ee3bfbf30a2b5b24aeaa78a5f850f2c5f63db
philipmir <philipmir@hotmail.com>
2023-04-04 10:38:27 +0200

    Initial Commit

---
+++ Stateintro/StateintroApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateintroApp.swift
+//  Stateintro
+//
+//  Created by Philip Miranda on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct StateintroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
